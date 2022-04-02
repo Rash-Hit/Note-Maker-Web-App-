c896db258f22052ad4503f71024b7b9c6e0c860a
Anubhav Jain <anubhavjain.jain0@gmail.com>
2022-04-02 13:05:25 +0530

    Complete Note Maker App

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PlaylistAddCheckCircleRoundedIcon from '@mui/icons-material/PlaylistAddCheckCircleRounded';
+function Header() {
+  return (
+    <header>
+      <h1> <PlaylistAddCheckCircleRoundedIcon sx={{ fontSize: 30 }} /> Note-Maker </h1>
+    </header>
+  );
+}
+
+export default Header;
